9daa44136ad638c1fc5ac5a9cae091b1270021b5
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-06-29 12:40:08 +0500

    Add frontend/src/pages/About.jsx

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AboutContent from '../components/AboutContent';
+
+function About() {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6">About Us</h1>
+      <AboutContent />
+    </div>
+  );
+}
+
+export default About;
